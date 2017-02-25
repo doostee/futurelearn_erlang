3afdb1b36112948d71eb7db47cf5e45eb6788852
Dustin Campbell <dustin.campbell@schneider-electric.com>
2017-02-25 12:47:40 -0600

    Updated formatting and comments.

--- week1.erl
+++ week1.erl
@@ -2,16 +2,10 @@
 -export([perimeter/1, area/1, enclose/1, bits_tail_recurse/1, bits_direct_recurse/1]).
 -include_lib("eunit/include/eunit.hrl").
 
-%% @doc square a number
-square(X) ->
-    X * X.
-
-%% @doc Calculate the distance between two cartesian coordinates
-distance({X1, Y1}, {X2, Y2}) ->
-    math:sqrt(square(X2 - X1) + square(Y2 - Y1)).
-
+%%
 %% @doc Calculate the perimeter of a shape
 %% @param shape Must be a tuple begining with 'circle', 'square', or 'triangle'
+%%
 perimeter({circle, {_X, _Y}, R}) when R >= 0 ->
     2*math:pi()*R;
 perimeter({rectangle, {_X, _Y}, H, W}) when (H >= 0) and (W >= 0) ->
@@ -19,8 +13,10 @@ perimeter({rectangle, {_X, _Y}, H, W}) when (H >= 0) and (W >= 0) ->
 perimeter({triangle, P1, P2, P3}) ->
     distance(P1, P2) + distance(P2, P3) + distance(P3, P1).
 
+%%
 %% @doc Calculate the area of a shape
 %% @param shape Must be a tuple begining with 'circle', 'square', or 'triangle'
+%%
 area({circle, {_X, _Y}, R})  when R >= 0 ->
     math:pi()*square(R);
 area({rectangle, {_X, _Y}, H, W}) when (H >= 0) and (W >= 0) ->
@@ -32,6 +28,60 @@ area({triangle, P1, P2, P3}) ->
     C = distance(P3, P1),
     S = (A + B + C)/2,
     math:sqrt(S*(S-A)*(S-B)*(S-C)).
+
+%%
+%% @doc get an enclosing rectangle of the passed shape
+%%
+enclose({circle, {X, Y}, R})  when R >= 0 ->
+    {rectangle, {X, Y}, 2*R, 2*R};
+enclose({rectangle, {X, Y}, H, W}) when (H >= 0) and (W >= 0) ->
+    {rectangle, {X, Y}, H, W};
+enclose({triangle, {X1, Y1}, {X2, Y2}, {X3, Y3}}) ->
+    XMIN = min(X1, X2, X3),
+    XMAX = max(X1, X2, X3),
+    YMIN = min(Y1, Y2, Y3),
+    YMAX = max(Y1, Y2, Y3),
+    {rectangle, {(XMAX - XMIN)/2, (YMAX - YMIN)/2}, YMAX - YMIN, XMAX - XMIN}.
+
+%%
+%% @doc Calculate the sum of the bits using tail recursion
+%% @param N must be a positive integer
+%% @returns an integer which is the sum of the bits
+%%
+bits_tail_recurse(N) when N > 0 ->
+    bits_tail_recurse(N, 0).
+
+bits_tail_recurse(0, P) ->
+    P;
+bits_tail_recurse(V, P) when V band 1 == 1 ->
+    bits_tail_recurse(V div 2, P + 1);
+bits_tail_recurse(V, P) ->
+    bits_tail_recurse(V div 2, P).
+
+%%
+%% @doc Calculate the sum of the bits using direct recursion
+%% @param N must be a positive integer
+%% @returns an integer which is the sum of the bits
+%%
+bits_direct_recurse(N) when N > 0 ->
+    bits_direct_recurse_sub(N).
+
+bits_direct_recurse_sub(N) when N == 0 ->
+    0;
+bits_direct_recurse_sub(N) ->
+    N band 1 + bits_direct_recurse_sub(N div 2).
+
+%%
+%% Private, not exported functions
+%%
+
+%% @doc square a number
+square(X) ->
+    X * X.
+
+%% @doc Calculate the distance between two cartesian coordinates
+distance({X1, Y1}, {X2, Y2}) ->
+    math:sqrt(square(X2 - X1) + square(Y2 - Y1)).
 
 %% @doc get the min of 3 numbers
 min(A, B, C) ->
@@ -51,43 +101,10 @@ max2(A, B) when A > B ->
 max2(_A, B) ->
     B.
 
-%% @doc get an enclosing rectangle of the passed shape
-enclose({circle, {X, Y}, R})  when R >= 0 ->
-    {rectangle, {X, Y}, 2*R, 2*R};
-enclose({rectangle, {X, Y}, H, W}) when (H >= 0) and (W >= 0) ->
-    {rectangle, {X, Y}, H, W};
-enclose({triangle, {X1, Y1}, {X2, Y2}, {X3, Y3}}) ->
-    XMIN = min(X1, X2, X3),
-    XMAX = max(X1, X2, X3),
-    YMIN = min(Y1, Y2, Y3),
-    YMAX = max(Y1, Y2, Y3),
-    {rectangle, {(XMAX - XMIN)/2, (YMAX - YMIN)/2}, YMAX - YMIN, XMAX - XMIN}.
 
-%% @doc Calculate the sum of the bits using tail recursion
-%% @param N must be a positive integer
-%% @returns an integer which is the sum of the bits
-bits_tail_recurse(N) when N > 0 ->
-    bits_tail_recurse(N, 0).
-
-bits_tail_recurse(0, P) ->
-    P;
-bits_tail_recurse(V, P) when V band 1 == 1 ->
-    bits_tail_recurse(V div 2, P + 1);
-bits_tail_recurse(V, P) ->
-    bits_tail_recurse(V div 2, P).
-
-%% @doc Calculate the sum of the bits using direct recursion
-%% @param N must be a positive integer
-%% @returns an integer which is the sum of the bits
-bits_direct_recurse(N) when N > 0 ->
-    bits_direct_recurse_sub(N).
-
-bits_direct_recurse_sub(N) when N == 0 ->
-    0;
-bits_direct_recurse_sub(N) ->
-    N band 1 + bits_direct_recurse_sub(N div 2).
-
+%%
 %% Unit Tests
+%%
 bits_tail_recurse_test() ->
     [?assertEqual(3, bits_tail_recurse(7)),
      ?assertEqual(1, bits_tail_recurse(8)),
